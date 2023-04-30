a5af37eef138aee5c84872101dfee135de00b1a8
JawahirKhan <nayabrasheedn@gmail.com>
2023-04-30 15:56:00 +0500

    Updated project

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ app.get("/about-route", ((req,res)=> {
     res.send('Hello From Jawahir');
 }))
 
+app.get("/contact", ((req,res)=> { 
+    res.send('<h1> My Contact </h1>');
+}))
+
+app.get("/home", ((req,res)=> { 
+    res.send('<h1> My Home Page </h1>');
+}))
+
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
 })
